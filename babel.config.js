fa4532deca504d476285d6ee3f1c5a023dcd8a74
JuiusNiedermeier <juius.niedermeier@outlook.de>
2024-04-30 23:48:44 +0200

    Added expo camera plugin and config to babel config

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,17 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ["babel-preset-expo"],
-    plugins: ["react-native-reanimated/plugin"],
+    plugins: [
+      "react-native-reanimated/plugin",
+      [
+        "expo-camera",
+        {
+          cameraPermission: "Allow $(PRODUCT_NAME) to access your camera",
+          microphonePermission:
+            "Allow $(PRODUCT_NAME) to access your microphone",
+          recordAudioAndroid: true,
+        },
+      ],
+    ],
   };
 };
