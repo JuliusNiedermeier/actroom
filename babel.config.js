d847fee510956eda9aecc146e5283b8d925e0f63
JuiusNiedermeier <juius.niedermeier@outlook.de>
2024-05-01 18:07:47 +0200

    Fixed expo camera setup expo-camera config should be located inside app.json instead of babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -2,17 +2,6 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ["babel-preset-expo"],
-    plugins: [
-      [
-        "expo-camera",
-        {
-          cameraPermission: "Allow $(PRODUCT_NAME) to access your camera",
-          microphonePermission:
-            "Allow $(PRODUCT_NAME) to access your microphone",
-          recordAudioAndroid: true,
-        },
-      ],
-      "react-native-reanimated/plugin",
-    ],
+    plugins: ["react-native-reanimated/plugin"],
   };
 };
