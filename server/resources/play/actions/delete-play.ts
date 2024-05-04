e163cf3410c192223dcfde711b9b77436811ba85
JuiusNiedermeier <juius.niedermeier@outlook.de>
2024-05-04 23:05:16 +0200

    Added source part file deletion on play deletion

--- server/resources/play/actions/delete-play.ts
+++ server/resources/play/actions/delete-play.ts
@@ -3,9 +3,11 @@ import { z } from "zod";
 import { playTable } from "@/server/resources/schema";
 import { drizzle } from "@/server/services/drizzle";
 import { eq } from "drizzle-orm";
+import { bucket } from "@/server/services/gcp";
 
 export const deletePlay = publicProcedure
   .input(z.object({ ID: z.string() }))
-  .mutation(({ input }) =>
-    drizzle.delete(playTable).where(eq(playTable.ID, input.ID))
-  );
+  .mutation(async ({ input }) => {
+    await drizzle.delete(playTable).where(eq(playTable.ID, input.ID));
+    await bucket.deleteFiles({ prefix: `source-parts/${input.ID}:` });
+  });
