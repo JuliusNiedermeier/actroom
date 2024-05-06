26175139517e9f588ef8be553b60c74e7baf316c
JuiusNiedermeier <juius.niedermeier@outlook.de>
2024-05-07 00:40:05 +0200

    Added order by position clause to getPlay trpc procedure

--- server/resources/play/actions/get-play.ts
+++ server/resources/play/actions/get-play.ts
@@ -1,14 +1,17 @@
 import { drizzle } from "@/server/services/drizzle";
 import { publicProcedure } from "@/server/trpc";
-import { eq } from "drizzle-orm";
+import { asc, eq } from "drizzle-orm";
 import { z } from "zod";
-import { playTable } from "@/server/resources/schema";
+import { blockTable, playTable } from "@/server/resources/schema";
 
 export const getPlay = publicProcedure
   .input(z.object({ ID: z.string() }))
   .query(({ input }) =>
     drizzle.query.playTable.findFirst({
       where: eq(playTable.ID, input.ID),
-      with: { sourceParts: true, blocks: true },
+      with: {
+        sourceParts: true,
+        blocks: { orderBy: asc(blockTable.position) },
+      },
     })
   );
