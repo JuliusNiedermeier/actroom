10559f2e4b756c5ed78874ae04f9cda0446a0a4a
JuiusNiedermeier <juius.niedermeier@outlook.de>
2024-05-06 12:38:25 +0200

    Renamed script patch-polyfills to patch-expo-server, because no polyfills are actually patched, but disabled in the @expo/server package. Thus the expo server is actually patched here.

--- scripts/patch-expo-server.ts
+++ scripts/patch-expo-server.ts
@@ -6,7 +6,7 @@ const content = readFileSync(path, "utf-8");
 
 const patchedContent = content.replace(
   "(0, node_1.installGlobals)();",
-  "// (0, node_1.installGlobals)(); // Line commented out by patch-polyfills script"
+  "// (0, node_1.installGlobals)(); // Line commented out by patch-expo-server script"
 );
 
 writeFileSync(path, patchedContent);
